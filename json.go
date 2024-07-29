d382367099de56f2cea33c08253b537472d0d487
gennesseaux <jocelyn@gennesseaux.fr>
2024-07-29 21:14:15 +0200

    complete rework on json conversion.

--- json.go
+++ json.go
@@ -1,59 +1,494 @@
 package convert
 
-import "encoding/json"
+import (
+	"encoding/json"
+	"fmt"
+)
 
-func ToJson(i interface{}) (d []byte, err error) {
-	d, err = json.Marshal(i)
-	return
+// JsonConvert is a function type that converts any value to a pointer to JSON byte slice.
+// If the conversion fails, it returns nil.
+// This allows for custom conversion logic to be implemented and passed to conversion functions.
+//
+// Examples:
+//
+//	customConverter := func(value interface{}) *[]byte {
+//		if v, ok := value.(CustomType); ok {
+//			b, err := json.Marshal(v)
+//			if err == nil {
+//				return &b
+//			}
+//		}
+//		return nil
+//	}
+//
+//	result := ToJson(myCustomValue, customConverter)
+type JsonConvert func(value interface{}) *[]byte
+
+// ToJson converts any type of value to JSON byte slice.
+// It uses ToJsonE internally and ignores any error.
+//
+// Parameters:
+//   - value: The value to be converted to JSON.
+//   - converters: Optional custom converters to be applied before the default conversion.
+//
+// Returns:
+//
+//	A JSON byte slice representation of the input value.
+//
+// Example:
+//
+//	b := ToJson(map[string]interface{}{"key": "value"})
+//	fmt.Println(string(b)) // Output: {"key":"value"}
+//
+//	customConverter := func(value interface{}) *[]byte {
+//		if v, ok := value.(CustomType); ok {
+//			b, err := json.Marshal(v)
+//			if err == nil {
+//				return &b
+//			}
+//		}
+//		return nil
+//	}
+//	customJson := ToJson(myCustomValue, customConverter)
+func ToJson(value interface{}, converters ...JsonConvert) []byte {
+	res, _ := ToJsonE(value, converters...)
+	return res
 }
 
-func ToJsonOrPanic(i interface{}) []byte {
-	if res, err := ToJson(i); err == nil {
-		return res
-	} else {
-		panic(err)
+// ToJsonOrDefault converts any type of value to JSON byte slice.
+// If the conversion fails, it returns the provided default value.
+//
+// Parameters:
+//   - value: The value to be converted to JSON.
+//   - defaultValue: The default value to return if conversion fails.
+//   - converters: Optional custom converters to be applied before the default conversion.
+//
+// Returns:
+//
+//	A JSON byte slice representation of the input value or the default value if conversion fails.
+//
+// Example:
+//
+//	b := ToJsonOrDefault(map[string]interface{}{"key": "value"}, []byte("{}"))
+//	fmt.Println(string(b)) // Output: {"key":"value"}
+//
+//	b = ToJsonOrDefault(nil, []byte("{}"))
+//	fmt.Println(string(b)) // Output: {}
+//
+//	customConverter := func(value interface{}) *[]byte {
+//		if v, ok := value.(CustomType); ok {
+//			b, err := json.Marshal(v)
+//			if err == nil {
+//				return &b
+//			}
+//		}
+//		return nil
+//	}
+//	customJson := ToJsonOrDefault(myCustomValue, []byte("{}"), customConverter)
+func ToJsonOrDefault(value interface{}, defaultValue []byte, converters ...JsonConvert) []byte {
+	if value == nil {
+		return defaultValue
 	}
-}
-
-func ToJsonString(i interface{}) (string, error) {
-	var d []byte
-	var err error
-	d, err = ToJson(i)
-	return string(d), err
-}
-
-func ToJsonStringOrPanic(i interface{}) string {
-	if res, err := ToJsonString(i); err == nil {
-		return res
-	} else {
-		panic(err)
+	res, err := ToJsonE(value, converters...)
+	if err != nil {
+		return defaultValue
 	}
+	return res
 }
 
-func ToJsonIndent(i interface{}) (d []byte, err error) {
-	d, err = json.MarshalIndent(i, "", "  ")
-	return
-}
-
-func ToJsonIndentOrPanic(i interface{}) string {
-	if res, err := ToJsonIndent(i); err == nil {
-		return string(res)
-	} else {
-		panic(err)
+// ToJsonE converts any type of value to JSON byte slice or returns an error.
+//
+// Parameters:
+//   - value: The value to be converted to JSON.
+//   - converters: Optional custom converters to be applied before the default conversion.
+//
+// Returns:
+//   - A JSON byte slice representation of the input value.
+//   - An error if the conversion fails.
+//
+// Example:
+//
+//	b, err := ToJsonE(map[string]interface{}{"key": "value"})
+//	if err != nil {
+//		log.Fatal(err)
+//	}
+//	fmt.Println(string(b)) // Output: {"key":"value"}
+//
+//	customConverter := func(value interface{}) *[]byte {
+//		if v, ok := value.(CustomType); ok {
+//			b, err := json.Marshal(v)
+//			if err == nil {
+//				return &b
+//			}
+//		}
+//		return nil
+//	}
+//	customJson, err := ToJsonE(myCustomValue, customConverter)
+func ToJsonE(value interface{}, converters ...JsonConvert) ([]byte, error) {
+	if value == nil {
+		return nil, fmt.Errorf("nil value cannot be converted to JSON")
 	}
-}
-
-func ToJsonIndentString(i interface{}) (string, error) {
-	var d []byte
-	var err error
-	d, err = ToJsonIndent(i)
-	return string(d), err
-}
-
-func ToJsonIndentStringOrPanic(i interface{}) string {
-	if res, err := ToJsonIndentString(i); err == nil {
-		return string(res)
-	} else {
-		panic(err)
+	for _, converter := range converters {
+		if result := converter(value); result != nil {
+			return *result, nil
+		}
 	}
+
+	b, err := json.Marshal(value)
+	if err != nil {
+		return nil, err
+	}
+	return b, nil
+}
+
+// ToJsonString converts any type of value to JSON string.
+// It uses ToJsonStringE internally and ignores any error.
+//
+// Parameters:
+//   - value: The value to be converted to JSON string.
+//   - converters: Optional custom converters to be applied before the default conversion.
+//
+// Returns:
+//
+//	A JSON string representation of the input value.
+//
+// Example:
+//
+//	s := ToJsonString(map[string]interface{}{"key": "value"})
+//	fmt.Println(s) // Output: {"key":"value"}
+//
+//	customConverter := func(value interface{}) *string {
+//		if v, ok := value.(CustomType); ok {
+//			b, err := json.Marshal(v)
+//			if err == nil {
+//				s := string(b)
+//				return &s
+//			}
+//		}
+//		return nil
+//	}
+//	customJson := ToJsonString(myCustomValue, customConverter)
+func ToJsonString(value interface{}, converters ...JsonConvert) string {
+	res, _ := ToJsonStringE(value, converters...)
+	return res
+}
+
+// ToJsonStringOrDefault converts any type of value to JSON string.
+// If the conversion fails, it returns the provided default value.
+//
+// Parameters:
+//   - value: The value to be converted to JSON string.
+//   - defaultValue: The default value to return if conversion fails.
+//   - converters: Optional custom converters to be applied before the default conversion.
+//
+// Returns:
+//
+//	A JSON string representation of the input value or the default value if conversion fails.
+//
+// Example:
+//
+//	s := ToJsonStringOrDefault(map[string]interface{}{"key": "value"}, "{}")
+//	fmt.Println(s) // Output: {"key":"value"}
+//
+//	s = ToJsonStringOrDefault(nil, "{}")
+//	fmt.Println(s) // Output: {}
+//
+//	customConverter := func(value interface{}) *string {
+//		if v, ok := value.(CustomType); ok {
+//			b, err := json.Marshal(v)
+//			if err == nil {
+//				s := string(b)
+//				return &s
+//			}
+//		}
+//		return nil
+//	}
+//	customJson := ToJsonStringOrDefault(myCustomValue, "{}", customConverter)
+func ToJsonStringOrDefault(value interface{}, defaultValue string, converters ...JsonConvert) string {
+	if value == nil {
+		return defaultValue
+	}
+	res, err := ToJsonStringE(value, converters...)
+	if err != nil {
+		return defaultValue
+	}
+	return res
+}
+
+// ToJsonStringE converts any type of value to JSON string or returns an error.
+//
+// Parameters:
+//   - value: The value to be converted to JSON string.
+//   - converters: Optional custom converters to be applied before the default conversion.
+//
+// Returns:
+//   - A JSON string representation of the input value.
+//   - An error if the conversion fails.
+//
+// Example:
+//
+//	s, err := ToJsonStringE(map[string]interface{}{"key": "value"})
+//	if err != nil {
+//		log.Fatal(err)
+//	}
+//	fmt.Println(s) // Output: {"key":"value"}
+//
+//	customConverter := func(value interface{}) *string {
+//		if v, ok := value.(CustomType); ok {
+//			b, err := json.Marshal(v)
+//			if err == nil {
+//				s := string(b)
+//				return &s
+//			}
+//		}
+//		return nil
+//	}
+//	customJson, err := ToJsonStringE(myCustomValue, customConverter)
+func ToJsonStringE(value interface{}, converters ...JsonConvert) (string, error) {
+	b, err := ToJsonE(value, converters...)
+	if err != nil {
+		return "", err
+	}
+	return string(b), nil
+}
+
+// ToJsonIndent converts any type of value to indented JSON byte slice.
+// It uses ToJsonIndentE internally and ignores any error.
+//
+// Parameters:
+//   - value: The value to be converted to indented JSON.
+//   - converters: Optional custom converters to be applied before the default conversion.
+//
+// Returns:
+//
+//	An indented JSON byte slice representation of the input value.
+//
+// Example:
+//
+//	b := ToJsonIndent(map[string]interface{}{"key": "value"})
+//	fmt.Println(string(b)) // Output: {
+//	//   "key": "value"
+//	// }
+//
+//	customConverter := func(value interface{}) *[]byte {
+//		if v, ok := value.(CustomType); ok {
+//			b, err := json.MarshalIndent(v, "", "  ")
+//			if err == nil {
+//				return &b
+//			}
+//		}
+//		return nil
+//	}
+//	customJson := ToJsonIndent(myCustomValue, customConverter)
+func ToJsonIndent(value interface{}, converters ...JsonConvert) []byte {
+	res, _ := ToJsonIndentE(value, converters...)
+	return res
+}
+
+// ToJsonIndentOrDefault converts any type of value to indented JSON byte slice.
+// If the conversion fails, it returns the provided default value.
+//
+// Parameters:
+//   - value: The value to be converted to indented JSON.
+//   - defaultValue: The default value to return if conversion fails.
+//   - converters: Optional custom converters to be applied before the default conversion.
+//
+// Returns:
+//
+//	An indented JSON byte slice representation of the input value or the default value if conversion fails.
+//
+// Example:
+//
+//	b := ToJsonIndentOrDefault(map[string]interface{}{"key": "value"}, []byte("{}"))
+//	fmt.Println(string(b)) // Output: {
+//	//   "key": "value"
+//	// }
+//
+//	b = ToJsonIndentOrDefault(nil, []byte("{}"))
+//	fmt.Println(string(b)) // Output: {}
+//
+//	customConverter := func(value interface{}) *[]byte {
+//		if v, ok := value.(CustomType); ok {
+//			b, err := json.MarshalIndent(v, "", "  ")
+//			if err == nil {
+//				return &b
+//			}
+//		}
+//		return nil
+//	}
+//	customJson := ToJsonIndentOrDefault(myCustomValue, []byte("{}"), customConverter)
+func ToJsonIndentOrDefault(value interface{}, defaultValue []byte, converters ...JsonConvert) []byte {
+	if value == nil {
+		return defaultValue
+	}
+	res, err := ToJsonIndentE(value, converters...)
+	if err != nil {
+		return defaultValue
+	}
+	return res
+}
+
+// ToJsonIndentE converts any type of value to indented JSON byte slice or returns an error.
+//
+// Parameters:
+//   - value: The value to be converted to indented JSON.
+//   - converters: Optional custom converters to be applied before the default conversion.
+//
+// Returns:
+//   - An indented JSON byte slice representation of the input value.
+//   - An error if the conversion fails.
+//
+// Example:
+//
+//	b, err := ToJsonIndentE(map[string]interface{}{"key": "value"})
+//	if err != nil {
+//		log.Fatal(err)
+//	}
+//	fmt.Println(string(b)) // Output: {
+//	//   "key": "value"
+//	// }
+//
+//	customConverter := func(value interface{}) *[]byte {
+//		if v, ok := value.(CustomType); ok {
+//			b, err := json.MarshalIndent(v, "", "  ")
+//			if err == nil {
+//				return &b
+//			}
+//		}
+//		return nil
+//	}
+//	customJson, err := ToJsonIndentE(myCustomValue, customConverter)
+func ToJsonIndentE(value interface{}, converters ...JsonConvert) ([]byte, error) {
+	if value == nil {
+		return nil, fmt.Errorf("nil value cannot be converted to JSON")
+	}
+
+	for _, converter := range converters {
+		if result := converter(value); result != nil {
+			return *result, nil
+		}
+	}
+
+	b, err := json.MarshalIndent(value, "", "  ")
+	if err != nil {
+		return nil, err
+	}
+	return b, nil
+}
+
+// ToJsonIndentString converts any type of value to indented JSON string.
+// It uses ToJsonIndentStringE internally and ignores any error.
+//
+// Parameters:
+//   - value: The value to be converted to indented JSON string.
+//   - converters: Optional custom converters to be applied before the default conversion.
+//
+// Returns:
+//
+//	An indented JSON string representation of the input value.
+//
+// Example:
+//
+//	s := ToJsonIndentString(map[string]interface{}{"key": "value"})
+//	fmt.Println(s) // Output: {
+//	//   "key": "value"
+//	// }
+//
+//	customConverter := func(value interface{}) *string {
+//		if v, ok := value.(CustomType); ok {
+//			b, err := json.MarshalIndent(v, "", "  ")
+//			if err == nil {
+//				s := string(b)
+//				return &s
+//			}
+//		}
+//		return nil
+//	}
+//	customJson := ToJsonIndentString(myCustomValue, customConverter)
+func ToJsonIndentString(value interface{}, converters ...JsonConvert) string {
+	res, _ := ToJsonIndentStringE(value, converters...)
+	return res
+}
+
+// ToJsonIndentStringOrDefault converts any type of value to indented JSON string.
+// If the conversion fails, it returns the provided default value.
+//
+// Parameters:
+//   - value: The value to be converted to indented JSON string.
+//   - defaultValue: The default value to return if conversion fails.
+//   - converters: Optional custom converters to be applied before the default conversion.
+//
+// Returns:
+//
+//	An indented JSON string representation of the input value or the default value if conversion fails.
+//
+// Example:
+//
+//	s := ToJsonIndentStringOrDefault(map[string]interface{}{"key": "value"}, "{}")
+//	fmt.Println(s) // Output: {
+//	//   "key": "value"
+//	// }
+//
+//	s = ToJsonIndentStringOrDefault(nil, "{}")
+//	fmt.Println(s) // Output: {}
+//
+//	customConverter := func(value interface{}) *string {
+//		if v, ok := value.(CustomType); ok {
+//			b, err := json.MarshalIndent(v, "", "  ")
+//			if err == nil {
+//				s := string(b)
+//				return &s
+//			}
+//		}
+//		return nil
+//	}
+//	customJson := ToJsonIndentStringOrDefault(myCustomValue, "{}", customConverter)
+func ToJsonIndentStringOrDefault(value interface{}, defaultValue string, converters ...JsonConvert) string {
+	if value == nil {
+		return defaultValue
+	}
+	res, err := ToJsonIndentStringE(value, converters...)
+	if err != nil {
+		return defaultValue
+	}
+	return res
+}
+
+// ToJsonIndentStringE converts any type of value to indented JSON string or returns an error.
+//
+// Parameters:
+//   - value: The value to be converted to indented JSON string.
+//   - converters: Optional custom converters to be applied before the default conversion.
+//
+// Returns:
+//   - An indented JSON string representation of the input value.
+//   - An error if the conversion fails.
+//
+// Example:
+//
+//	s, err := ToJsonIndentStringE(map[string]interface{}{"key": "value"})
+//	if err != nil {
+//		log.Fatal(err)
+//	}
+//	fmt.Println(s) // Output: {
+//	//   "key": "value"
+//	// }
+//
+//	customConverter := func(value interface{}) *string {
+//		if v, ok := value.(CustomType); ok {
+//			b, err := json.MarshalIndent(v, "", "  ")
+//			if err == nil {
+//				s := string(b)
+//				return &s
+//			}
+//		}
+//		return nil
+//	}
+//	customJson, err := ToJsonIndentStringE(myCustomValue, customConverter)
+func ToJsonIndentStringE(value interface{}, converters ...JsonConvert) (string, error) {
+	b, err := ToJsonIndentE(value, converters...)
+	if err != nil {
+		return "", err
+	}
+	return string(b), nil
 }
--- json_test.go
+++ json_test.go
@@ -1,38 +1,218 @@
-package convert_test
+package convert
 
 import (
-	"github.com/go-mods/convert"
+	"encoding/json"
+	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
+type CustomType struct {
+	Field string
+}
+
+func (c CustomType) MarshalJSON() ([]byte, error) {
+	return json.Marshal(map[string]string{"custom_field": c.Field})
+}
+
 func TestToJson(t *testing.T) {
+	// Test with a simple map
+	value := map[string]interface{}{"key": "value"}
+	expected := `{"key":"value"}`
+	result := ToJson(value)
+	assert.JSONEq(t, expected, string(result))
 
-	type Person struct {
-		FirstName string `json:"first_name"`
-		LastName  string `json:"last_name"`
-		FullName  string `json:"full_name"`
+	// Test with a custom converter
+	customConverter := func(value interface{}) *[]byte {
+		if v, ok := value.(CustomType); ok {
+			b, err := json.Marshal(v)
+			if err == nil {
+				return &b
+			}
+		}
+		return nil
 	}
-	var person = &Person{FirstName: "first", LastName: "last", FullName: "full"}
-	var personStr = "{\"first_name\":\"first\",\"last_name\":\"last\",\"full_name\":\"full\"}"
-	var personIndenStr = "{\n  \"first_name\": \"first\",\n  \"last_name\": \"last\",\n  \"full_name\": \"full\"\n}"
+	customValue := CustomType{Field: "test"}
+	expectedCustom := `{"custom_field":"test"}`
+	resultCustom := ToJson(customValue, customConverter)
+	assert.JSONEq(t, expectedCustom, string(resultCustom))
 
-	personConvertStr, err := convert.ToJsonString(person)
-	if err != nil {
-		t.Errorf("ToJsonString() error = %v", err)
-		return
-	}
+	// Test with nil value
+	resultNil := ToJson(nil)
+	assert.Nil(t, resultNil)
+}
 
-	if personStr != personConvertStr {
-		t.Errorf("ToJson() got = %v, want %v", personConvertStr, personStr)
-	}
+func TestToJsonOrDefault(t *testing.T) {
+	// Test with a simple map
+	value := map[string]interface{}{"key": "value"}
+	defaultValue := []byte(`{}`)
+	expected := `{"key":"value"}`
+	result := ToJsonOrDefault(value, defaultValue)
+	assert.JSONEq(t, expected, string(result))
 
-	personConvertIndentStr, err := convert.ToJsonIndentString(person)
-	if err != nil {
-		t.Errorf("ToJsonIndentString() error = %v", err)
-		return
-	}
+	// Test with nil value
+	resultDefault := ToJsonOrDefault(nil, defaultValue)
+	assert.JSONEq(t, string(defaultValue), string(resultDefault))
+}
 
-	if personIndenStr != personConvertIndentStr {
-		t.Errorf("ToJson() got = %v, want %v", personConvertIndentStr, personIndenStr)
-	}
+func TestToJsonE(t *testing.T) {
+	// Test with a simple map
+	value := map[string]interface{}{"key": "value"}
+	expected := `{"key":"value"}`
+	result, err := ToJsonE(value)
+	assert.NoError(t, err)
+	assert.JSONEq(t, expected, string(result))
+
+	// Test with an invalid value
+	invalidValue := make(chan int)
+	_, err = ToJsonE(invalidValue)
+	assert.Error(t, err)
+
+	// Test with nil value
+	resultNil, err := ToJsonE(nil)
+	assert.Error(t, err)
+	assert.Nil(t, resultNil)
+}
+
+func TestToJsonString(t *testing.T) {
+	// Test with a simple map
+	value := map[string]interface{}{"key": "value"}
+	expected := `{"key":"value"}`
+	result := ToJsonString(value)
+	assert.JSONEq(t, expected, result)
+
+	// Test with nil value
+	resultNil := ToJsonString(nil)
+	assert.Equal(t, "", resultNil)
+}
+
+func TestToJsonStringOrDefault(t *testing.T) {
+	// Test with a simple map
+	value := map[string]interface{}{"key": "value"}
+	defaultValue := "{}"
+	expected := `{"key":"value"}`
+	result := ToJsonStringOrDefault(value, defaultValue)
+	assert.JSONEq(t, expected, result)
+
+	// Test with nil value
+	resultDefault := ToJsonStringOrDefault(nil, defaultValue)
+	assert.JSONEq(t, defaultValue, resultDefault)
+}
+
+func TestToJsonStringE(t *testing.T) {
+	// Test with a simple map
+	value := map[string]interface{}{"key": "value"}
+	expected := `{"key":"value"}`
+	result, err := ToJsonStringE(value)
+	assert.NoError(t, err)
+	assert.JSONEq(t, expected, result)
+
+	// Test with an invalid value
+	invalidValue := make(chan int)
+	_, err = ToJsonStringE(invalidValue)
+	assert.Error(t, err)
+
+	// Test with nil value
+	resultNil, err := ToJsonStringE(nil)
+	assert.Error(t, err)
+	assert.Equal(t, "", resultNil)
+}
+
+func TestToJsonIndent(t *testing.T) {
+	// Test with a simple map
+	value := map[string]interface{}{"key": "value"}
+	expected := `{
+  "key": "value"
+}`
+	result := ToJsonIndent(value)
+	assert.JSONEq(t, expected, string(result))
+
+	// Test with nil value
+	resultNil := ToJsonIndent(nil)
+	assert.Nil(t, resultNil)
+}
+
+func TestToJsonIndentOrDefault(t *testing.T) {
+	// Test with a simple map
+	value := map[string]interface{}{"key": "value"}
+	defaultValue := []byte(`{}`)
+	expected := `{
+  "key": "value"
+}`
+	result := ToJsonIndentOrDefault(value, defaultValue)
+	assert.JSONEq(t, expected, string(result))
+
+	// Test with nil value
+	resultDefault := ToJsonIndentOrDefault(nil, defaultValue)
+	assert.JSONEq(t, string(defaultValue), string(resultDefault))
+}
+
+func TestToJsonIndentE(t *testing.T) {
+	// Test with a simple map
+	value := map[string]interface{}{"key": "value"}
+	expected := `{
+  "key": "value"
+}`
+	result, err := ToJsonIndentE(value)
+	assert.NoError(t, err)
+	assert.JSONEq(t, expected, string(result))
+
+	// Test with an invalid value
+	invalidValue := make(chan int)
+	_, err = ToJsonIndentE(invalidValue)
+	assert.Error(t, err)
+
+	// Test with nil value
+	resultNil, err := ToJsonIndentE(nil)
+	assert.Error(t, err)
+	assert.Nil(t, resultNil)
+}
+
+func TestToJsonIndentString(t *testing.T) {
+	// Test with a simple map
+	value := map[string]interface{}{"key": "value"}
+	expected := `{
+  "key": "value"
+}`
+	result := ToJsonIndentString(value)
+	assert.JSONEq(t, expected, result)
+
+	// Test with nil value
+	resultNil := ToJsonIndentString(nil)
+	assert.Equal(t, "", resultNil)
+}
+
+func TestToJsonIndentStringOrDefault(t *testing.T) {
+	// Test with a simple map
+	value := map[string]interface{}{"key": "value"}
+	defaultValue := "{}"
+	expected := `{
+  "key": "value"
+}`
+	result := ToJsonIndentStringOrDefault(value, defaultValue)
+	assert.JSONEq(t, expected, result)
+
+	// Test with nil value
+	resultDefault := ToJsonIndentStringOrDefault(nil, defaultValue)
+	assert.JSONEq(t, defaultValue, resultDefault)
+}
+
+func TestToJsonIndentStringE(t *testing.T) {
+	// Test with a simple map
+	value := map[string]interface{}{"key": "value"}
+	expected := `{
+  "key": "value"
+}`
+	result, err := ToJsonIndentStringE(value)
+	assert.NoError(t, err)
+	assert.JSONEq(t, expected, result)
+
+	// Test with an invalid value
+	invalidValue := make(chan int)
+	_, err = ToJsonIndentStringE(invalidValue)
+	assert.Error(t, err)
+
+	// Test with nil value
+	resultNil, err := ToJsonIndentStringE(nil)
+	assert.Error(t, err)
+	assert.Equal(t, "", resultNil)
 }
